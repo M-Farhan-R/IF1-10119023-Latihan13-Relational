cf2d3eac5bd1b269c750116e6e72133de21bb51a
Farhan <farhan.10119023@mahasiswa.unikom.ac.id>
2020-10-20 20:37:15 +0700

    Latihan 13 - Relational

---
+++ src/if1/pkg10119023/latihan13/relational/IF110119023Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119023.latihan13.relational;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhammad Farhan R
+ * KELAS    : IF1
+ * NIM      : 10119023
+ * Deskripsi Program : latihan menggunakan symbol untuk menyatakan relasi antar
+ * variabel
+ */
+public class IF110119023Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bi11 = 10;
+        int bi12 = 20;
+        System.out.println("a == b = " + (bi11 == bi12));
+        System.out.println("a != b = " + (bi11 != bi12));
+        System.out.println("a > b = " + (bi11 > bi12));
+        System.out.println("a < b = " + (bi11 < bi12));
+        System.out.println("b >= a = " + (bi11 >= bi12));
+        System.out.println("b <= a = " + (bi11 <= bi12));
+    }
+    
+}
